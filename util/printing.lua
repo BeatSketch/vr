d765718efa95b3a1037bb881174cded9e793521d
Janis Hutz <development@janishutz.com>
2026-05-06 18:41:16 +0200

    feat: table print now supports all dtypes

--- util/printing.lua
+++ util/printing.lua
@@ -1,37 +1,48 @@
 local M = {}
 
 --- Print a table (recursively)
---- @param t table the table to print
+--- @param t any the table to print
 function M.print_table(t)
-    --- Print helper
-    ---@param t table the table to print
-    ---@param idx integer The index (depth)
-    local function table_print_helper(t, idx)
-        local out = ""
-        if rawget(t, 1) ~= nil or next(t) == nil then
-            -- Is array
-            for _, value in pairs(t) do
-                if type(value) == "table" then
-                    out = out .. table_print_helper(value, idx + 1) .. "\n"
-                else
-                    out =  out .. (" "):rep((idx + 1) * 2) .. value .. "\n"
-                end
-            end
-            return (" "):rep(idx * 2) .. "[\n" .. out .. (" "):rep(idx * 2) .. "]"
-        else
-            -- is dict
-            for key, value in pairs(t) do
-                if type(value) == "table" then
-                    out = out .. table_print_helper(value, idx + 1) .. "\n"
-                else
-                    out = out .. (" "):rep((idx + 1) * 2) .. key .. ": " .. value .. "\n"
-                end
-            end
-            return (" "):rep(idx * 2) .. "{\n" .. out .. (" "):rep(idx * 2) .. "}"
-        end
-    end
+	if t == nil then
+		print("{}")
+		return
+	end
 
-    print(table_print_helper(t, 0))
+	if type(t) == "table" then
+		--- Print helper
+		---@param data table the table to print
+		---@param idx integer The index (depth)
+		local function table_print_helper(data, idx)
+			local out = ""
+			if rawget(data, 1) ~= nil or next(data) == nil then
+				-- Is array
+				for _, value in pairs(data) do
+					if type(value) == "table" then
+						out = out .. table_print_helper(value, idx + 1) .. "\n"
+					else
+						out = out .. (" "):rep((idx + 1) * 2) .. value .. "\n"
+					end
+				end
+				return (" "):rep(idx * 2) .. "[\n" .. out .. (" "):rep(idx * 2) .. "]"
+			else
+				-- is dict
+				for key, value in pairs(data) do
+					if type(value) == "table" then
+						out = out .. table_print_helper(value, idx + 1) .. "\n"
+					else
+						out = out .. (" "):rep((idx + 1) * 2) .. key .. ": " .. value .. "\n"
+					end
+				end
+				return (" "):rep(idx * 2) .. "{\n" .. out .. (" "):rep(idx * 2) .. "}"
+			end
+		end
+
+		print(table_print_helper(t, 0))
+    elseif type(t) == "number" or type(t) == "string" or type(t) == "boolean" then
+        print(t)
+    else
+        print("UNSUPPORTED TYPE PASSED", type(t))
+	end
 end
 
 return M
