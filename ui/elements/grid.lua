d0d270ec5b014b86f79505677e7aed49d1b1a814
Janis Hutz <development@janishutz.com>
2026-05-18 11:05:21 +0200

    chore(format): Format new code

--- ui/elements/grid.lua
+++ ui/elements/grid.lua
@@ -5,54 +5,66 @@ local M = {}
 --- Draw a grid for possible colornote positions
 --- @param pass Pass
 M.grid = function(pass)
-    local m, n = 3, 4
-    --- Amount of grid layers
-    local d = math.floor(state.bpm * (state.len / 60)) * state.beat_div
-    local size = 0.666
-    --- Distance between grid layers
-    local dist = 60 / state.bpm * state.spd / state.beat_div
-    local o = {
-        x = -1.333,
-        y = 0,
-        z = 0       --- Must start here to align with song bpm's 0
-    }
-    o.z = o.z + state.disp
+	local m, n = 3, 4
+	--- Amount of grid layers
+	local d = math.floor(state.bpm * (state.len / 60)) * state.beat_div
+	local size = 0.666
+	--- Distance between grid layers
+	local dist = 60 / state.bpm * state.spd / state.beat_div
+	local o = {
+		x = -1.333,
+		y = 0,
+		z = 0, --- Must start here to align with song bpm's 0
+	}
+	o.z = o.z + state.disp
 
-    local color_normal = {0.1, 0.1, 0.1, 1}
-    local color_highlight = {0.2, 0.2, 0.2, 1}
+	local color_normal = { 0.1, 0.1, 0.1, 1 }
+	local color_highlight = { 0.2, 0.2, 0.2, 1 }
 
-    pass:setColor(color_normal)
-    for i = 0, n, 1 do
-        for j = 0, m, 1 do
-            local points = {
-                o.x + i*size, o.y + j*size, o.z,
-                o.x + i*size, o.y + j*size, o.z - d*dist
-            }
-            pass:line(points)
-        end
-    end
+	pass:setColor(color_normal)
+	for i = 0, n, 1 do
+		for j = 0, m, 1 do
+			local points = {
+				o.x + i * size,
+				o.y + j * size,
+				o.z,
+				o.x + i * size,
+				o.y + j * size,
+				o.z - d * dist,
+			}
+			pass:line(points)
+		end
+	end
 
-    for k = 0, d, 1 do
-        if (k % state.beat_div == 0) then
-            pass:setColor(color_highlight)
-        else
-            pass:setColor(color_normal)
-        end
-        for i = 0, n, 1 do
-            local points = {
-                o.x + i*size, o.y, o.z - k*dist;
-                o.x + i*size, o.y + m*size, o.z - k*dist
-            }
-            pass:line(points)
-        end
-        for j = 0, m, 1 do
-            local points = {
-                o.x, o.y + j*size, o.z - k*dist,
-                o.x + n*size, o.y + j*size, o.z - k*dist
-            }
-            pass:line(points)
-        end
-    end
+	for k = 0, d, 1 do
+		if k % state.beat_div == 0 then
+			pass:setColor(color_highlight)
+		else
+			pass:setColor(color_normal)
+		end
+		for i = 0, n, 1 do
+			local points = {
+				o.x + i * size,
+				o.y,
+				o.z - k * dist,
+				o.x + i * size,
+				o.y + m * size,
+				o.z - k * dist,
+			}
+			pass:line(points)
+		end
+		for j = 0, m, 1 do
+			local points = {
+				o.x,
+				o.y + j * size,
+				o.z - k * dist,
+				o.x + n * size,
+				o.y + j * size,
+				o.z - k * dist,
+			}
+			pass:line(points)
+		end
+	end
 end
 
 return M
--- ui/elements/handlines.lua
+++ ui/elements/handlines.lua
@@ -4,46 +4,47 @@ local M = {}
 --- Draw a visualisation of the current tracking history
 --- @param pass Pass
 M.draw = function(pass)
-    local data = state.history:get()
+	local data = state.history:get()
 
-    local l_color_normal = {1, 0, 0, 1}
-    local l_color_highlight = {0.5, 0, 0, 1}
+	local l_color_normal = { 1, 0, 0, 1 }
+	local l_color_highlight = { 0.5, 0, 0, 1 }
 
-    local r_color_normal = {0, 0, 1, 1}
-    local r_color_highlight = {0, 0, 0.5, 1}
+	local r_color_normal = { 0, 0, 1, 1 }
+	local r_color_highlight = { 0, 0, 0.5, 1 }
 
-    M.draw_line(pass, data.left, l_color_normal, l_color_highlight)
-    M.draw_line(pass, data.right, r_color_normal, r_color_highlight)
+	M.draw_line(pass, data.left, l_color_normal, l_color_highlight)
+	M.draw_line(pass, data.right, r_color_normal, r_color_highlight)
 end
 
 --- Draw a visualisation for a single hand line
 --- @param pass Pass
---- @param arr table<number, PositionState>
+--- @param arr PositionStates
 --- @param color_normal table<number, number>
 --- @param color_highlight table<number, number>
 M.draw_line = function(pass, arr, color_normal, color_highlight)
-    local n = #arr
-    if (n == 0) then return end
+	local n = #arr
+	if n == 0 then
+		return
+	end
 
-    -- Treat first explicitly
-    local point = arr[1].pos + lovr.math.vec3(0, 0, state.disp - state.history_disp[1])
-    pass:setColor(color_highlight)
-    pass:points(point)
+	-- Treat first explicitly
+	local point = arr[1].pos + lovr.math.vec3(0, 0, state.disp - state.history_disp[1])
+	pass:setColor(color_highlight)
+	pass:points(point)
 
-    local prev = point
-    for i = 2, n, 1 do
-        -- Need to be shifted by grid displacement at time of recording and current grid disp
-        point = arr[i].pos + lovr.math.vec3(0, 0, state.disp - state.history_disp[i])
+	local prev = point
+	for i = 2, n, 1 do
+		-- Need to be shifted by grid displacement at time of recording and current grid disp
+		point = arr[i].pos + lovr.math.vec3(0, 0, state.disp - state.history_disp[i])
 
-        pass:setColor(color_highlight)
-        pass:points(point)
+		pass:setColor(color_highlight)
+		pass:points(point)
 
-        pass:setColor(color_normal)
-        pass:line(prev, point)
+		pass:setColor(color_normal)
+		pass:line(prev, point)
 
-        prev = point
-    end
+		prev = point
+	end
 end
-
 
 return M
