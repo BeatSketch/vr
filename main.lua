3e1fcddb179054a98c89e3028f70987f2c5592b4
Janis Hutz <development@janishutz.com>
2026-05-21 11:07:25 +0200

    chore: license stuff, remove unused code

--- main.lua
+++ main.lua
@@ -1,3 +1,29 @@
+--[[
+     ___               _   ___   _           _         _
+    (  _ \            ( )_(  _ \( )         ( )_      ( )
+    | (_) )  __    _ _|  _) (_(_) |/ )   __ |  _)  ___| |__
+    |  _ ( / __ \/ _  ) |  \__ \|   (  / __ \ |  / ___)  _  \
+    | (_) )  ___/ (_| | |_( )_) | |\ \(  ___/ |_( (___| | | |
+    (____/ \____)\__ _)\__)\____)_) (_)\____)\__)\____)_) (_)
+
+
+    A VR Beat Saber Map maker allowing you to simply play the map to create it
+    Copyright (C) 2026 BeatSketch Maintainers
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--]]
+
 local sabers = require("ui.controllers.sabers")
 local render = require("ui.render")
 local ipc = require("util.ipc.main")
@@ -10,43 +36,33 @@ local blocks = require("ui.elements.blocks")
 local args = cli.parse_cli_opts()
 local launch_with_launcher = args["launcher"] and args["launcher"] == "true"
 if not args["song"] then
-	local cwd = os.getenv("PWD")
-	args["song"] = cwd .. "/test/audio.mp3"
-	args["mirror"] = "true"
+    local cwd = os.getenv("PWD")
+    args["song"] = cwd .. "/test/audio.mp3"
+    args["mirror"] = "true"
 end
-
---[[
- ___               _   ___   _           _         _
-(  _ \            ( )_(  _ \( )         ( )_      ( )
-| (_) )  __    _ _|  _) (_(_) |/ )   __ |  _)  ___| |__
-|  _ ( / __ \/ _  ) |  \__ \|   (  / __ \ |  / ___)  _  \
-| (_) )  ___/ (_| | |_( )_) | |\ \(  ___/ |_( (___| | | |
-(____/ \____)\__ _)\__)\____)_) (_)\____)\__)\____)_) (_)
-
---]]
 
 -- ┌                                               ┐
 -- │                 Configuration                 │
 -- └                                               ┘
 function lovr.conf(t)
-	-- We can also start the headset session later on (by calling lovr.headset.start and do a desktop UI first)
-	-- I have yet to figure out the resizing and stuff
-	t.headset.start = true
-	t.identity = "beatsketch"
-	t.window.width = 1920
-	t.window.resizable = true
+    -- We can also start the headset session later on (by calling lovr.headset.start and do a desktop UI first)
+    -- I have yet to figure out the resizing and stuff
+    t.headset.start = true
+    t.identity = "beatsketch"
+    t.window.width = 1920
+    t.window.resizable = true
 end
 
 -- ┌                                               ┐
 -- │        Load audio file (and textures)         │
 -- └                                               ┘
 function lovr.load()
-	if args["song"] then
-		audio.load(args["song"])
-	else
-		print("\n[WARNING] No song specified, thus no audio was loaded")
-	end
-	blocks.load_texture()
+    if args["song"] then
+        audio.load(args["song"])
+    else
+        print("\n[WARNING] No song specified, thus no audio was loaded")
+    end
+    blocks.load_texture()
 end
 
 -- ┌                                               ┐
@@ -54,33 +70,21 @@ end
 -- └                                               ┘
 -- Drawing the screen is called once every frame
 function lovr.draw(pass)
-	sabers.draw(pass)
-	render.draw(pass)
-	blocks.draw(pass)
+    sabers.draw(pass)
+    render.draw(pass)
+    blocks.draw(pass)
 end
 
-for i = 0, 7, 1 do
-	blocks.add_block({
-		x = 2,
-		y = 2,
-		beat = 0.5 * i + 0.5,
-		hand = "left",
-		orientation = i,
-	})
-end
-
-local x, y, z = -3, 3, 3
-local view = lovr.math.newMat4():lookAt(vec3(x, y, z), vec3(0, 0, 0))
--- TODO: Possibly a separate desktop mirror
--- (to draw it from 3rd person instead, could be a good effect for demo,
--- and doesn't look to be hard: https://lovr.org/docs/Flatscreen/Spectator_Camera)
+-- Desktop mirror. Can be disabled
 if not args["mirror"] or args["mirror"] == "false" then
-	function lovr.mirror(pass)
-		pass:transform(view)
-		sabers.draw(pass)
-		render.draw(pass)
-		return false
-	end
+    local x, y, z = -3, 3, 3
+    local view = lovr.math.newMat4():lookAt(vec3(x, y, z), vec3(0, 0, 0))
+    function lovr.mirror(pass)
+        pass:transform(view)
+        sabers.draw(pass)
+        render.draw(pass)
+        return false
+    end
 end
 
 -- ┌                                               ┐
@@ -88,7 +92,8 @@ end
 -- └                                               ┘
 -- Tracking and the like get continuous updates
 function lovr.update(delta_time)
-	updates.update_handler(delta_time, launch_with_launcher)
+    updates.update_handler(delta_time, launch_with_launcher)
 end
 
+updates.configure(args)
 ipc.init(launch_with_launcher)
